d02cfac4c429705e73a707234739e73eddcaee35
JMcunst <xnslqjtmghaz@naver.com>
2021-03-11 18:58:33 +0900

    feat: add calendar functions

--- app/src/main/java/coms/example/fivesensediary/ui/sharedDiary/SharedDiaryFragment.java
+++ app/src/main/java/coms/example/fivesensediary/ui/sharedDiary/SharedDiaryFragment.java
@@ -1,10 +1,13 @@
 package coms.example.fivesensediary.ui.sharedDiary;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,9 +15,26 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.CalendarMode;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+
+import java.util.Calendar;
+import java.util.concurrent.Executors;
+
+import coms.example.fivesensediary.MainActivity;
 import coms.example.fivesensediary.R;
+import coms.example.fivesensediary.decorators.OneDayDecorator;
+import coms.example.fivesensediary.decorators.SaturdayDecorator;
+import coms.example.fivesensediary.decorators.SundayDecorator;
 
 public class SharedDiaryFragment extends Fragment {
+
+    String time,kcal,menu;
+    private final OneDayDecorator oneDayDecorator = new OneDayDecorator();
+    Cursor cursor;
+    MaterialCalendarView materialCalendarView;
 
     private SharedDiaryViewModel sharedDiaryViewModel;
 
@@ -23,11 +43,47 @@ public class SharedDiaryFragment extends Fragment {
         sharedDiaryViewModel =
                 new ViewModelProvider(this).get(SharedDiaryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_shared_diary, container, false);
-        final TextView textView = root.findViewById(R.id.text_shared_diary);
+
+        materialCalendarView = (MaterialCalendarView)root.findViewById(R.id.calendarView);
+
+        materialCalendarView.state().edit()
+                .setFirstDayOfWeek(Calendar.SUNDAY)
+                .setMinimumDate(CalendarDay.from(2017, 0, 1)) // 달력의 시작
+                .setMaximumDate(CalendarDay.from(2030, 11, 31)) // 달력의 끝
+                .setCalendarDisplayMode(CalendarMode.MONTHS)
+                .commit();
+
+        materialCalendarView.addDecorators(
+                new SundayDecorator(),
+                new SaturdayDecorator(),
+                oneDayDecorator);
+
+        String[] result = {"2021,01,11","2021,02,11","2021,03,11","2021,04,11"};
+
+        materialCalendarView.setOnDateChangedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+                int Year = date.getYear();
+                int Month = date.getMonth() + 1;
+                int Day = date.getDay();
+
+                Log.i("Year test", Year + "");
+                Log.i("Month test", Month + "");
+                Log.i("Day test", Day + "");
+
+                String shot_Day = Year + "," + Month + "," + Day;
+
+                Log.i("shot_Day test", shot_Day + "");
+                materialCalendarView.clearSelection();
+
+                Toast.makeText(getContext(), shot_Day , Toast.LENGTH_SHORT).show();
+            }
+        });
+
         sharedDiaryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+
             }
         });
         return root;
